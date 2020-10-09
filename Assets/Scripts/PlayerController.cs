ff703be263f8474449f32dcf07c63fa4813026ae
Dejan Popov <dekipopica92@gmail.com>
2020-10-09 21:52:38 +0200

    Made player movement script
    - Added rigidbody to script in Unity

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Rigidbody is component for charater movement.We reference that.
+    //Slide Rigidbody into the script we made in Unity.
+    public Rigidbody2D rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+} 
+
+/*Rigidbody is component for character movement.We use reference for that.*/

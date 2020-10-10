fc10bfd1241a28fc88a656a518a4e8062ea70b34
Dejan Popov <dekipopica92@gmail.com>
2020-10-10 15:52:20 +0200

    Player movement script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -38,5 +38,14 @@ public class PlayerController : MonoBehaviour
         //Setting float values ( like we set moveX and moveY in Unity).
         animator.SetFloat("moveX", rigidBody.velocity.x);
         animator.SetFloat("moveY", rigidBody.velocity.y);
+
+        //Storing the last value if player moved the character,
+        //so aniamtor will know whick animations to trigger.
+        if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1
+            || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+        {
+            animator.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+            animator.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+        }
     }
 } 

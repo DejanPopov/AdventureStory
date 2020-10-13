6bb6b66d4dec59d1e61569503e91b8c0de85da75
Dejan Popov <dekipopica92@gmail.com>
2020-10-13 13:36:36 +0200

    PlayerLoader
    - Loads player into scenes
    -Still has disappearing player bug

---
+++ Assets/Scripts/PlayerLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLoader : MonoBehaviour
+{
+    //We want to load gameObject (that is player).
+    //Thats why we dont use here PlayerController.
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerController.instance == null)
+        {
+            //If there is no player in this world,instantiate one.
+            Instantiate(player);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

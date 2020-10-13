df5a5ea263930f84717acdf87d6b44f4d8129707
Dejan Popov <dekipopica92@gmail.com>
2020-10-13 10:48:05 +0200

    Entry/Exit point is bugged
    - i did something with Prefab that should be Entry/Exit point of the map.
    - Player spawn when transitioning is random

---
+++ Assets/Scripts/AreaEntrance.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaEntrance : MonoBehaviour
+{
+    //For player to not to spawn in the middle of map.
+    public string transtionName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //If player transtion name is as this transition name
+        if (transtionName == PlayerController.instance.areaTransitionName)
+        {
+            //Then take position from player and put it where this script is attached.
+            //more likely find this player and put him at this point.
+            PlayerController.instance.transform.position = transform.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

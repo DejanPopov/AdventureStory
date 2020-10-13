010e58598b0825f3470e81aa2944c52bcb9176b1
Dejan Popov <dekipopica92@gmail.com>
2020-10-13 13:48:55 +0200

    Carea is following player
    - In Test not in Test2 yet.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //Find postion of player and move camera to that position
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Finding target (player)
+        target = PlayerController.instance.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Set X,Y,Z values
+        //Z value is camera. If we would make transform.position.z
+        //into target.position.z, the camera would be too close to the game
+        //and everything would be blue.
+        transform.position = new Vector3(target.position.x,
+            target.position.y, transform.position.z);
+    }
+}

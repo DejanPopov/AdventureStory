48e9a4d881455273caa7b0c538a44f9a1e33f7ca
Dejan Popov <dekipopica92@gmail.com>
2020-10-14 14:40:09 +0200

    Fading (Loading screen)
    - Everything is black now

---
+++ Assets/Scripts/UIFade.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//UI
+using UnityEngine.UI;
+
+public class UIFade : MonoBehaviour
+{
+    public static UIFade instance;
+
+    //We are taking Alpha value of picture colour
+    public Image fadeScreen;
+    public float fadeSpeed;
+
+    private bool shouldFadeToBlack;
+    private bool shouldFadeFromBlack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shouldFadeToBlack)
+        {
+            //Mathf.MoveTowards is gona push something in setted speed
+            //DelaTime is amount of time it took to the last update to current update.
+            fadeScreen.color = new Color(fadeScreen.color.r, fadeScreen.color.g,
+                fadeScreen.color.b, Mathf.MoveTowards(fadeScreen.color.a, 1f, 
+                fadeSpeed * Time.deltaTime));
+
+            if (fadeScreen.color.a == 1f)
+            {
+                shouldFadeToBlack = false;
+            }
+        }
+
+        if (shouldFadeFromBlack)
+        {
+            fadeScreen.color = new Color(fadeScreen.color.r, fadeScreen.color.g,
+                fadeScreen.color.b, Mathf.MoveTowards(fadeScreen.color.a, 0f,
+                fadeSpeed * Time.deltaTime));
+
+            if (fadeScreen.color.a == 1f)
+            {
+                shouldFadeFromBlack = false;
+            }
+        }
+    }
+
+    public void FadeToBlack()
+    {
+        shouldFadeToBlack = true;
+        shouldFadeFromBlack = false;
+    }
+
+    public void FadeFromBlack()
+    {
+        shouldFadeToBlack = false;
+        shouldFadeFromBlack = true;
+    }
+}

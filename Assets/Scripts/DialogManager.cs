464b1ee6681586155a10bc223ee84395e0ea4b8a
Dejan Popov <dekipopica92@gmail.com>
2020-10-20 09:07:04 +0200

    Dialog lines script

---
+++ Assets/Scripts/DialogManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//UI
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour
+{
+    public Text dialogText;
+    public Text nameText;
+    public GameObject dialogBox;
+    public GameObject nameBox;
+
+    //Array of lines
+    public string[] dialogLines;
+
+    public int currentLine;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogText.text = dialogLines[currentLine];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
